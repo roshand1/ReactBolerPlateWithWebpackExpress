e0d5cfee10ed781d8a25648202d9373ceb5139a6
Roshan Kandel <rkandel@healthgrades.com>
2021-02-15 14:44:16 -0700

    created dynamic table grid

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,116 +1,37 @@
 import './table.css';
 
-function Table() {
+function Table(props) {
+    const {headers, data} = props;
+    var columnHeader = [];
+    
+    const displayHeader = () => headers.map((header,i) => 
+    {
+        columnHeader.push(Object.keys(header)[0]);
+        return <div className="text">{header[Object.keys(header)[0]]}</div>
+    });
+
+    const displayData = () => data.map((d,i) => {
+        return (<div className="wrapper text-2 table-row wrapper">
+            {columnHeader.map(h => {
+                    return <div className="text">{d[h]}</div>
+                })}
+            </div>)
+        }
+    );
+    
     return (
         <div className="container-fluid">
-        <div className="table-row wrapper header">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">First Name</div>
-              <div className="text">Last Name</div>
+          <div className="table-row wrapper header">
+            <div className="wrapper text-4">
+              <div className="wrapper text-2">
+                {displayHeader()}
+              </div>
             </div>
-            <div className="wrapper text-2">
-              <div className="text">Email</div>
-              <div className="text">Birthday</div>
             </div>
-          </div>
-          </div>
-
-        <div className="table-row wrapper">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">Vasan</div>
-              <div className="text">Subramanian</div>
+            <div className="wrapper text-4">
+                {displayData()}
             </div>
-            <div className="wrapper text-2">
-              <div className="text" title="vasan@example.com">vasan@example.com</div>
-              <div className="text">January 14, 1996</div>
-            </div>
-          
-            <div className="container-fluid inside-table">
-        <div className="table-row wrapper inside-table-header">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">First Name</div>
-              <div className="text">Last Name</div>
-            </div>
-            <div className="wrapper text-2">
-              <div className="text">Email</div>
-              <div className="text">Birthday</div>
-            </div>
-          </div>
-          </div>
-
-        <div className="table-row wrapper">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">Vasan</div>
-              <div className="text">Subramanian</div>
-            </div>
-            <div className="wrapper text-2">
-              <div className="text" title="vasan@example.com">vasan@example.com</div>
-              <div className="text">January 14, 1996</div>
-            </div>
-          </div>
         </div>
-
-        <div className="table-row wrapper">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">Firstname</div>
-              <div className="text">Lastname</div>
-            </div>
-            <div className="wrapper text-2">
-              <div className="text" title="ns@example.org">ns@example.org</div>
-              <div className="text">February 31, 1948</div>
-            </div>
-          </div>
-        </div>
-        
-        <div className="table-row wrapper">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">Example</div>
-              <div className="text">McDude</div>
-            </div>
-            <div className="wrapper text-2">
-              <div className="text" title="N/A">--</div>
-              <div className="text">December 31, 1960</div>
-            </div>
-          </div>
-        </div>
-  </div>
-          
-          
-          </div>
-        </div>
-
-        <div className="table-row wrapper">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">Firstname</div>
-              <div className="text">Lastname</div>
-            </div>
-            <div className="wrapper text-2">
-              <div className="text" title="ns@example.org">ns@example.org</div>
-              <div className="text">February 31, 1948</div>
-            </div>
-          </div>
-        </div>
-        
-        <div className="table-row wrapper">
-          <div className="wrapper text-4">
-            <div className="wrapper text-2">
-              <div className="text">Example</div>
-              <div className="text">McDude</div>
-            </div>
-            <div className="wrapper text-2">
-              <div className="text" title="N/A">--</div>
-              <div className="text">December 31, 1960</div>
-            </div>
-          </div>
-        </div>
-  </div>
     );
   }
   export default Table;
--- src/components/Table.stories.js
+++ src/components/Table.stories.js
@@ -2,7 +2,40 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Table from './Table.jsx';
 
+var props = {
+  headers:[
+    {firstName:'FirstName'}, 
+    {lastName:'LastName'},
+    {email:'Email'},
+    {gender: 'Gender'}],
+  data:[{
+    firstName:'Alok',
+    lastName:'Shrestha',
+    email:'as@hgweb.com',
+    gender:'Male',
+  },{
+    firstName:'Prakash',
+    lastName:'Shrestha',
+    email:'ps@hgweb.com',
+    gender:'Male',
+  },{
+    firstName:'Utsav ',
+    lastName:'Siwakoti',
+    email:'as@hgweb.com',
+    gender:'Male',
+  },{
+    firstName:'Anjita',
+    lastName:'Thapa',
+    email:'at@hgweb.com',
+    gender:'Female',
+  },{
+    firstName:'Neelam',
+    lastName:'Shrestha',
+    email:'ns@hgweb.com',
+    gender:'Female',
+  }]
+}
 
 storiesOf('Table', module).add('Table', () => (
-    <Table/>
+    <Table {...props}/>
   ));
