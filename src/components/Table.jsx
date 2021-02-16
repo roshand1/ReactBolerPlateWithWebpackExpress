63b779b687b3e7df5240b7063f257cb6234b9c95
Roshan Kandel <rkandel@healthgrades.com>
2021-02-15 21:47:00 -0700

    added new table in css

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import classNames from "classnames";
 import './table.css';
 import './Table-Dynamic'
 
@@ -14,37 +15,49 @@ function Table(props) {
         }else{ 
         columnHeader.push(Object.keys(header)[0]);
         }
-        console.log(columnHeader);
         return <div className="text">{header[Object.keys(header)[0]]}</div>
     });
 
-    const createDataRow = (columnHeader, row) => columnHeader.map(h => {
+    const createDataRow = (columnHeader, row, hasNestedTable) => columnHeader.map(h => {
+        if(hasNestedTable) return <button className="toggle-down-button text">{row[h]}</button>
         return <div className="text">{row[h]}</div>
     })
 
     const displayData = (dataToDisplay, isNestedTable) => dataToDisplay.map((d,i) => {
         return (<div className="wrapper text-2 table-row wrapper">
-            {isNestedTable ? createDataRow(nestedColumnHeader,d): createDataRow(columnHeader,d)}
+            {isNestedTable ? createDataRow(nestedColumnHeader,d,false): createDataRow(columnHeader,d,d.hasNestedTable)}
                 {d.hasNestedTable && renderTable(d.nestedData.headers, d.nestedData.data,true)}
             </div>)
         }
     );
 
     
-    const renderTable = (headersToDisplay, dataToDisplay,isNestedTable) => (
-        <div className="container-fluid">
-                <div className="table-row wrapper header">
+    const renderTable = (headersToDisplay, dataToDisplay,isNestedTable) => {
+        const classes = classNames({
+            'container-fluid': true,
+            'inside-table':isNestedTable
+          });
+
+        const headerClasses = classNames({
+            'table-row wrapper': true,
+            'inside-table-header':isNestedTable,
+            'header':!isNestedTable
+          });
+           
+        return(
+            <div className={classes}>
+                <div className={headerClasses}>
                     <div className="wrapper text-4">
-                    <div className="wrapper text-2">
-                        {displayHeader(headersToDisplay,isNestedTable)}
-                    </div>
-                    </div>
-                    </div>
-                    <div className="wrapper text-4">
-                        {displayData(dataToDisplay,isNestedTable)}
+                        <div className="wrapper text-2">
+                            {displayHeader(headersToDisplay,isNestedTable)}
+                        </div>
                     </div>
                 </div>
-    )
+                <div className="wrapper text-4">
+                        {displayData(dataToDisplay,isNestedTable)}
+                </div>
+            </div>
+    )}
 
     return (
         <div>{renderTable(headers, data,false)}</div>
