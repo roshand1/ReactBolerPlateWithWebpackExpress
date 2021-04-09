dd9d6029ddaa3de700cd135ed2869d957d6fac0d
Roshan Kandel <rkandel@healthgrades.com>
2021-04-09 12:26:04 -0600

    fixed Dockerfile

--- express.js
+++ express.js
@@ -18,7 +18,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/app/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
